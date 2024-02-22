2cb1b3fb59dc68b9de559620b1c7ce37c0a15b28
Jon <j.gutierrez@ehu.eus>
2024-02-22 12:06:25 +0100

    updated analyzeResults.py to include some additional information

--- analyzeResults.py
+++ analyzeResults.py
@@ -13,7 +13,7 @@ carpetaGT = "/workspace/Vitis-AI/tutorials/HSI-DRIVE/files/workspace/baseDeDatos
 # Sacar cuántas veces se ha hecho flip en cada uno de los píxeles en cada una de las capas
 
 files = glob.glob(carpetaPrediccionFI + "*.png")
-bitFlipPositions = np.zeros((100, 32), dtype=int) #100 capas (not quantized) o 56 capas (quantized), 9 bitFlipPositions (ojo el MSB esta a la derecha) #56 capas para el modelo cuantizado
+bitFlipPositions = np.zeros((100, 32), dtype=int) #100 capas (not quantized) o 56 capas (quantized o folded), 9 bitFlipPositions (ojo el MSB esta a la derecha) #56 capas para el modelo cuantizado
 for file in files:
     filename = file.split("/")[-1].split(".")[0] #nfXXXX_XXX_YYYY_YYY_YY o #nfXXXX_XXX_patch_XXXX_YYYY_YYY_YY
     layerIdx = int(filename.split("_")[3])       #                 3     o                              5
@@ -21,27 +21,27 @@ for file in files:
     bitFlipPositions[layerIdx][bitIdx] += 1
 
 
-#files = glob.glob(carpetaPrediccionFI + "*.png")
-#files.sort()
-#bitFlipError = np.zeros((100, 32), dtype=int) #100 capas (not quantized) o 56 capas (quantized), 9 bitFlipPositions (ojo el MSB esta a la derecha)
-#bitFlipErrorReduced = np.zeros((100, 32), dtype=int) #100 capas (not quantized) o 56 capas (quantized), 9 bitFlipPositions (ojo el MSB esta a la derecha)
-#bitFlipErrorReduced2 = np.zeros((100, 32), dtype=int) #100 capas (not quantized) o 56 capas (quantized), 9 bitFlipPositions (ojo el MSB esta a la derecha)
-#for file in files:
-#   filename = file.split("/")[-1].split(".")[0] #nfXXXX_XXX_YY_YYY_YY
-#   prediccionFI = np.array(cv2.imread(file, 1)[:, : , 0])
-#     prediccionNFI = np.array(cv2.imread(carpetaPrediccionNFI + "/" + filename.split("_")[0] + "_" + filename.split("_")[1] + ".png", 1)[:, : , 0])
-#     labelGT = np.array(cv2.imread(carpetaGT + "/" + filename.split("_")[0] + "_" + filename.split("_")[1] + ".png", 1)[:, : , 0])
-#     prediccionNFI = np.array(cv2.imread(carpetaPrediccionNFI + "/" + filename.split("_")[0] + "_" + filename.split("_")[1] + "_"  + filename.split("_")[2] + "_" + filename.split("_")[3] + ".png", 1)[:, : , 0])
-#     labelGT = np.array(cv2.imread(carpetaGT + "/" + filename.split("_")[0] + "_" + filename.split("_")[1] + "_" + filename.split("_")[2] + "_" + filename.split("_")[3] + ".png", 1)[:, : , 0])
+files = glob.glob(carpetaPrediccionFI + "*.png")
+files.sort()
+bitFlipError = np.zeros((100, 32), dtype=int) #100 capas (not quantized) o 56 capas (quantized  o folded), 9 bitFlipPositions (ojo el MSB esta a la derecha)
+bitFlipErrorReduced = np.zeros((100, 32), dtype=int) #100 capas (not quantized) o 56 capas (quantized  o folded), 9 bitFlipPositions (ojo el MSB esta a la derecha)
+bitFlipErrorReduced2 = np.zeros((100, 32), dtype=int) #100 capas (not quantized) o 56 capas (quantized  o folded), 9 bitFlipPositions (ojo el MSB esta a la derecha)
+for file in files:
+   filename = file.split("/")[-1].split(".")[0] #nfXXXX_XXX_YY_YYY_YY
+   prediccionFI = np.array(cv2.imread(file, 1)[:, : , 0])
+   prediccionNFI = np.array(cv2.imread(carpetaPrediccionNFI + "/" + filename.split("_")[0] + "_" + filename.split("_")[1] + ".png", 1)[:, : , 0])
+   labelGT = np.array(cv2.imread(carpetaGT + "/" + filename.split("_")[0] + "_" + filename.split("_")[1] + ".png", 1)[:, : , 0])
+   prediccionNFI = np.array(cv2.imread(carpetaPrediccionNFI + "/" + filename.split("_")[0] + "_" + filename.split("_")[1] + "_"  + filename.split("_")[2] + "_" + filename.split("_")[3] + ".png", 1)[:, : , 0])
+   labelGT = np.array(cv2.imread(carpetaGT + "/" + filename.split("_")[0] + "_" + filename.split("_")[1] + "_" + filename.split("_")[2] + "_" + filename.split("_")[3] + ".png", 1)[:, : , 0])
 
-#     layerIdx = int(filename.split("_")[3])
-#     bitIdx = int(filename.split("_")[4])
+   layerIdx = int(filename.split("_")[3])
+   bitIdx = int(filename.split("_")[4])
 
-#     bitFlipError[layerIdx][bitIdx] += np.sum((prediccionNFI != prediccionFI))
-#     bitFlipErrorReduced[layerIdx][bitIdx] += np.sum(prediccionNFI[8:, 7:] != prediccionFI[8:, 7:])
-#     bitFlipErrorReduced2[layerIdx][bitIdx] += np.sum((prediccionNFI != prediccionFI) & (labelGT != 0))
+   bitFlipError[layerIdx][bitIdx] += np.sum((prediccionNFI != prediccionFI))
+   bitFlipErrorReduced[layerIdx][bitIdx] += np.sum(prediccionNFI[8:, 7:] != prediccionFI[8:, 7:])
+   bitFlipErrorReduced2[layerIdx][bitIdx] += np.sum((prediccionNFI != prediccionFI) & (labelGT != 0))
 
-np.save("/workspace/Vitis-AI/tutorials/HSI-DRIVE/files/workspace/baseDeDatos/HSI-Drive_2.0_Jon/Fold4/bitFlipPositionsPruned.npy", bitFlipPositions)
-#np.save("/workspace/Vitis-AI/tutorials/HSI-DRIVE/files/workspace/baseDeDatos/HSI-Drive_2.0_Jon/Fold4/bitFlipErrorPruned.npy", bitFlipError)
-#np.save("/workspace/Vitis-AI/tutorials/HSI-DRIVE/files/workspace/baseDeDatos/HSI-Drive_2.0_Jon/Fold4/bitFlipErrorReducedPruned.npy", bitFlipErrorReduced)
-#np.save("/workspace/Vitis-AI/tutorials/HSI-DRIVE/files/workspace/baseDeDatos/HSI-Drive_2.0_Jon/Fold4/bitFlipErrorReduced2Pruned.npy", bitFlipErrorReduced2)
+np.save("/workspace/Vitis-AI/tutorials/HSI-DRIVE/files/workspace/baseDeDatos/HSI-Drive_2.0_Jon/Fold4/bitFlipPositionsNotPruned.npy", bitFlipPositions)
+#np.save("/workspace/Vitis-AI/tutorials/HSI-DRIVE/files/workspace/baseDeDatos/HSI-Drive_2.0_Jon/Fold4/bitFlipErrorNotPruned.npy", bitFlipError)
+np.save("/workspace/Vitis-AI/tutorials/HSI-DRIVE/files/workspace/baseDeDatos/HSI-Drive_2.0_Jon/Fold4/bitFlipErrorNotReducedPruned.npy", bitFlipErrorReduced)
+np.save("/workspace/Vitis-AI/tutorials/HSI-DRIVE/files/workspace/baseDeDatos/HSI-Drive_2.0_Jon/Fold4/bitFlipErrorNotReduced2Pruned.npy", bitFlipErrorReduced2)
